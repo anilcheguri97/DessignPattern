7e0b138eadc02604ba861148dddbb62e739460e6
Anil <anilcheguri97@gmail.com>
2024-11-15 10:32:59 +0530

    afgn

--- CompositeDesignPattern/src/Main.java
+++ CompositeDesignPattern/src/Main.java
@@ -1,5 +1,21 @@
+import model.Directory;
+import model.File;
+import model.FileSystem;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Directory directory = new Directory("Movie");
+        FileSystem comedyMovie = new File("Boarder");
+        directory.add(comedyMovie);
+        Directory comedyMovieDirectory = new Directory("ComedyMovie");
+        FileSystem comedyMovie1 = new File("Hulbul");
+        comedyMovieDirectory.add(comedyMovie1);
+        comedyMovieDirectory.add(directory);
+
+        comedyMovieDirectory.ls();
+
+
+
+
     }
 }
--- CompositeDesignPattern/src/model/FileSystem.java
+++ CompositeDesignPattern/src/model/FileSystem.java
@@ -1,5 +1,5 @@
 package model;
 
 public interface FileSystem {
-
+public  void ls();
 }
